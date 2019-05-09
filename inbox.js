6529d43a2d71413762e10dd141c0c87010b76d52
Parag Chauhan <parag99799@gmail.com>
2019-05-09 19:11:29 +0530

    threadView for side pannel

--- inbox.js
+++ inbox.js
@@ -4,9 +4,20 @@ InboxSDK.load('1', 'sdk_MailContact_b5e5169e77').then((sdk) => {
         console.log('Message View :: ', messageView);
         var contact = messageView.getRecipientEmailAddresses();
         console.log('Contact :: ', contact);
-
-        messageView.getRecipientsFull().then((list) => {
-            console.log('Contact Full List:: ', list);
+        sdk.Conversations.registerThreadViewHandler((threadView) => {
+            console.log('Thread View :: ', threadView);
+            const createdElement = document.createElement('div');
+            var contactDiv = '<div>';
+            for(let x = 0 ; x < contact.length ; x++){
+                contactDiv = contactDiv + '<div>' + contact[x] + '</div>'
+            };
+            contactDiv = contactDiv + '</div>';
+            createdElement.innerHTML = contactDiv;
+            threadView.addSidebarContentPanel({
+                title: 'Contact List',
+                createdElement,
+            });
         });
     });
+    
 });
--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-var parent = document.getElementById('gbw').childNodes[1].childNodes[1].childNodes[0];
+// var parent = document.getElementById('gbw').childNodes[1].childNodes[1].childNodes[0];
 
-var button = document.createElement("button");
-text = document.createTextNode("Contact");
-button.appendChild(text);
+// var button = document.createElement("button");
+// text = document.createTextNode("Contact");
+// button.appendChild(text);
 
-parent.appendChild(button);
+// parent.appendChild(button);
