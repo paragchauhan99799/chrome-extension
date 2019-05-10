369715ef7a924513d4411e38f88e3b6042a680a5
Parag Chauhan <parag99799@gmail.com>
2019-05-10 19:18:15 +0530

    contact details from all thread

--- inbox.js
+++ inbox.js
@@ -1,23 +1,70 @@
+var threadViewHandler;
+var contentViewPannel;
+var contactListObject = {};
+
 InboxSDK.load('1', 'sdk_MailContact_b5e5169e77').then((sdk) => {
-	console.log('SDK :: ', sdk);
-    sdk.Conversations.registerMessageViewHandler((messageView) => {
-        console.log('Message View :: ', messageView);
-        var contact = messageView.getRecipientEmailAddresses();
-        console.log('Contact :: ', contact);
-        sdk.Conversations.registerThreadViewHandler((threadView) => {
-            console.log('Thread View :: ', threadView);
-            const createdElement = document.createElement('div');
-            var contactDiv = '<div>';
-            for(let x = 0 ; x < contact.length ; x++){
-                contactDiv = contactDiv + '<div>' + contact[x] + '</div>'
-            };
-            contactDiv = contactDiv + '</div>';
-            createdElement.innerHTML = contactDiv;
-            threadView.addSidebarContentPanel({
-                title: 'Contact List',
-                createdElement,
-            });
+  let contact;
+  sdk.Conversations.registerMessageViewHandler((messageView) => {
+    console.log('MSG State :: ', messageView.getViewState());
+    if(messageView.getViewState() == 'EXPANDED'){
+      let senderInfo = messageView.getSender();
+      contactListObject[senderInfo.emailAddress] = senderInfo.name;
+      messageView.getRecipientsFull().then((recipientinfo) => {
+        recipientinfo.forEach(element => {
+          console.log('element :: ', element);
+          if(element.name){
+            contactListObject[element.emailAddress] = element.name;
+          } else{
+            let getName = (element.emailAddress).split('@');
+            contactListObject[element.emailAddress] = getName[0];
+          }
         });
-    });
-    
+        showSidebarWithContact();
+      }).catch((error) => {
+        showSidebarWithContact();
+      })
+    }
+  });
+
+  sdk.Conversations.registerThreadViewHandler((threadView) => {
+    contactListObject = {};
+    threadViewHandler = threadView;
+    var allMsg = threadView.getMessageViewsAll();
+    console.log('All Msg :: ', allMsg);
+    for(let y = 0 ; y < allMsg.length ; y++){
+        let thisMsg = allMsg[y];
+        if(!(thisMsg.getViewState() == 'HIDDEN')){
+          console.log("Index :: ", y+1);
+          let senderInfo = thisMsg.getSender();
+          console.log('Sender Info :: ', senderInfo);
+          contactListObject[senderInfo.emailAddress] = senderInfo.name;
+          thisMsg.getRecipientsFull().then((recipientinfo) => {
+            recipientinfo.forEach(element => {
+              console.log('element in :: ', element);
+              contactListObject[element.emailAddress] = element.name;
+            });
+          })
+        }
+    }
+    showSidebarWithContact();
+  });
 });
+
+const showSidebarWithContact = () => {
+  if(contentViewPannel){
+    contentViewPannel.remove();
+  }
+  console.log('contactListObject :: ', contactListObject);
+  let createdElement = document.createElement("div");
+  createdElement.style = "padding-left: 20px"
+  let contactDiv = '<div>';
+  for(let key of Object.keys(contactListObject)){
+    contactDiv += '<div style="font-size: 14px; font-weight: bold; padding: 5px 0px; color: green"><span style="padding-right:3px">' + (1) + ') </span>' + '<span>' + contactListObject[key] + '</span><span>' + key + '</span></div>';
+  };
+  contactDiv += '</div>';
+  createdElement.innerHTML = contactDiv;
+  contentViewPannel = threadViewHandler.addSidebarContentPanel({
+    title: 'Contact List',
+    el:createdElement,
+  });
+}
--- main.js
+++
@@ -1,7 +0,0 @@
-// var parent = document.getElementById('gbw').childNodes[1].childNodes[1].childNodes[0];
-
-// var button = document.createElement("button");
-// text = document.createTextNode("Contact");
-// button.appendChild(text);
-
-// parent.appendChild(button);
