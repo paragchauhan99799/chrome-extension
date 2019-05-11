da5cba5a074f92f95e607303ab3edb417f7f5019
Parag Chauhan <parag99799@gmail.com>
2019-05-11 10:50:10 +0530

    get contact list with no name handled

--- inbox.js
+++ inbox.js
@@ -3,20 +3,17 @@ var contentViewPannel;
 var contactListObject = {};
 
 InboxSDK.load('1', 'sdk_MailContact_b5e5169e77').then((sdk) => {
-  let contact;
   sdk.Conversations.registerMessageViewHandler((messageView) => {
-    console.log('MSG State :: ', messageView.getViewState());
     if(messageView.getViewState() == 'EXPANDED'){
       let senderInfo = messageView.getSender();
       contactListObject[senderInfo.emailAddress] = senderInfo.name;
       messageView.getRecipientsFull().then((recipientinfo) => {
-        recipientinfo.forEach(element => {
-          console.log('element :: ', element);
-          if(element.name){
-            contactListObject[element.emailAddress] = element.name;
+        recipientinfo.forEach(recipient => {
+          if(recipient.name){
+            contactListObject[recipient.emailAddress] = recipient.name;
           } else{
-            let getName = (element.emailAddress).split('@');
-            contactListObject[element.emailAddress] = getName[0];
+            let getName = (recipient.emailAddress).split('@');
+            contactListObject[recipient.emailAddress] = getName[0];
           }
         });
         showSidebarWithContact();
@@ -30,21 +27,17 @@ InboxSDK.load('1', 'sdk_MailContact_b5e5169e77').then((sdk) => {
     contactListObject = {};
     threadViewHandler = threadView;
     var allMsg = threadView.getMessageViewsAll();
-    console.log('All Msg :: ', allMsg);
     for(let y = 0 ; y < allMsg.length ; y++){
-        let thisMsg = allMsg[y];
-        if(!(thisMsg.getViewState() == 'HIDDEN')){
-          console.log("Index :: ", y+1);
-          let senderInfo = thisMsg.getSender();
-          console.log('Sender Info :: ', senderInfo);
-          contactListObject[senderInfo.emailAddress] = senderInfo.name;
-          thisMsg.getRecipientsFull().then((recipientinfo) => {
-            recipientinfo.forEach(element => {
-              console.log('element in :: ', element);
-              contactListObject[element.emailAddress] = element.name;
-            });
-          })
-        }
+      let mailDetails = allMsg[y];
+      if(!(mailDetails.getViewState() == 'HIDDEN')){
+        let senderInfo = mailDetails.getSender();
+        contactListObject[senderInfo.emailAddress] = senderInfo.name;
+        mailDetails.getRecipientsFull().then((recipientinfo) => {
+          recipientinfo.forEach(recipient => {
+            contactListObject[recipient.emailAddress] = recipient.name;
+          });
+        })
+      }
     }
     showSidebarWithContact();
   });
@@ -54,12 +47,11 @@ const showSidebarWithContact = () => {
   if(contentViewPannel){
     contentViewPannel.remove();
   }
-  console.log('contactListObject :: ', contactListObject);
   let createdElement = document.createElement("div");
   createdElement.style = "padding-left: 20px"
   let contactDiv = '<div>';
   for(let key of Object.keys(contactListObject)){
-    contactDiv += '<div style="font-size: 14px; font-weight: bold; padding: 5px 0px; color: green"><span style="padding-right:3px">' + (1) + ') </span>' + '<span>' + contactListObject[key] + '</span><span>' + key + '</span></div>';
+    contactDiv += '<div style="font-size: 14px; font-weight: bold; padding: 5px 0px; color: green"><span style="padding-right:5px">' + contactListObject[key] + '</span><span> : ' + key + '</span></div>';
   };
   contactDiv += '</div>';
   createdElement.innerHTML = contactDiv;
